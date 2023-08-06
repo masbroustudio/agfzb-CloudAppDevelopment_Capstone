a732df9962e9a0d5272eb4f211a9f52b2adb776e
Yudha E <masbroumail@gmail.com>
2023-08-06 09:41:05 -0400

    Create About and Contact page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,10 +19,26 @@ urlpatterns = [
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
-
     # path for dealer reviews view
 
     # path for add a review view
+
+    path(route='about', view=views.about, name='about'),
+
+    path(route='contact', view=views.contact, name='contact'),
+
+    # path(route='registration', view=views.registration_request, name='registration'),
+
+    # path(route='login', view=views.login_request, name='login'),
+
+    # path(route='logout', view=views.logout_request, name='logout'),
+
+    path(route='', view=views.get_dealerships, name='index'),
+
+    # path('dealer/<int:dealer_id>/',
+    #      view=views.get_dealer_details, name='dealer_details'),
+
+    # path('review/<int:dealer_id>/',
+    #      view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
